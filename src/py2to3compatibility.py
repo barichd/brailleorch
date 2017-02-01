54892d5cc4bf8e2998609a5e8be1e73884d51ccb
Bo-cheng Jan <school510587@yahoo.com.tw>
2017-02-01 15:24:17 +0800

    py2to3compatibility.py: Add dict_keys / dict_iterkeys

--- src/py2to3compatibility.py
+++ src/py2to3compatibility.py
@@ -1,4 +1,12 @@
 try:
+    dict.iterkeys
+    dict_iterkeys = lambda d: d.iterkeys()
+    dict_keys = lambda d: d.keys()
+except AttributeError:
+    dict_iterkeys = lambda d: d.keys()
+    dict_keys = lambda d: list(d.keys())
+
+try:
     dict.itervalues
     dict_itervalues = lambda d: d.itervalues()
     dict_values = lambda d: d.values()
